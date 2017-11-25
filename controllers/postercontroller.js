0d5a048c42d64f48f2a9dfe472b1f756a04e0a9a
Landon Vago-Hughes <landon2008@hotmail.com>
2017-11-25 22:51:40 +0000

    sorted

--- controllers/postercontroller.js
+++ controllers/postercontroller.js
@@ -42,6 +42,17 @@ exports.postfile = function(req, res) {
         })
 
         console.log(fileinstance)
+
+        var child_process = require('child_process');
+
+        child_process.exec('python Analyse.py', function (err, stdout, stderr){
+            if (err) {
+                console.log("child processes failed with error code: " + err.code);
+            }else{
+                console.log(stdout)
+            }
+        });
+
         return res.json({})
 
 // Decrypt
